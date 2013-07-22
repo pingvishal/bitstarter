6ab769745746a79f456c1adcc310146427bbc8fd
Ubuntu <ubuntu@ip-172-31-29-248.us-west-2.compute.internal>
2013-07-22 16:47:44 +0000

    committing the mirror

--- web.js
+++ web.js
@@ -3,7 +3,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  fs.readFileSync('./index.html',function (err,data) {
+    if(err) throw err;
+    response.send(data.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
